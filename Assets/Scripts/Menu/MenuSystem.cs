a2804019b80f6932b1748535841a12fe53b048f5
Eidan Cárdenas <ecardenasv@uniempresarial.edu.co>
2025-11-18 07:33:29 -0500

    Primer commit subiendo el juego

---
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+ 
+public class MenuSystem : MonoBehaviour {
+
+    public void jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void salir()
+    {
+        Debug.Log("Saliendo del juego..."); 
+        Application.Quit();
+    }
+    
+}
---
+++ Assets/Scripts/PausarJuego.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PausarJuego : MonoBehaviour
+{
+    public GameObject menuPausa;
+    public bool juegoPausado = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (!juegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+
+    }
+
+    public void Reanudar()
+    {
+        menuPausa.SetActive(false);
+        Time.timeScale = 1;
+        juegoPausado = false;
+    }
+
+    public void Pausar()
+    {
+        menuPausa.SetActive(true);
+        Time.timeScale = 0;
+        juegoPausado = true;
+    }
+
+}
